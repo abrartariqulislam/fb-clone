236907394b25143e4debfae7d9fd83985b96d9c2
abrartariqulislam <islamtariqul010@gmail.com>
2023-12-11 23:28:45 +0600

    update

--- server/server.js
+++ server/server.js
@@ -2,45 +2,48 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 const dotenv = require("dotenv");
 dotenv.config();
 
 // app initialize
 const app = express();
-const port = process.env.PORT || 8000;
 
 // middleware
 app.use(express.json());
+app.use(morgan("dev"));
 
 // configuring cors with dynamic origin
 const whitelist = ["http://localhost:3000", "http://localhost:3303"];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+const corsOptionsDelegate = (req, callback) => {
+  let corsOptions;
+  if (whitelist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true, optionsSuccessStatus: 200 };
+  } else {
+    corsOptions = { origin: "Not allowed by CORS" };
+  }
+  callback(null, corsOptions);
 };
-app.use(cors(corsOptions));
+app.use(cors(corsOptionsDelegate));
 
 // routes
 app.get("/", (req, res) => {
   console.log("called");
   res.send({
-    name:"Tariqul Islam added from server"
-  })
+    name: "Tariqul Islam added from server",
+  });
 });
-
-
 
 // connect mongoose
-mongoose.connect(process.env.DB_URI).then(() => {
-  console.log("Connected mongoDB database");
-});
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => {
+    console.log("Connected mongoDB database");
+  })
+  .catch((err) => console.log("Error connect to mongoDB"));
 
 // connect server
+const port = process.env.PORT || 8000;
 app.listen(port, () =>
   console.log(`server is running @http://localhost:${port} `)
 );

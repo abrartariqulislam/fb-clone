79f15af685735c31792a4926b9f2ae8fbad7d52d
abrartariqulislam <islamtariqul010@gmail.com>
2024-01-09 22:56:56 +0600

    update register data validatoin

--- server/middlewares/validators.js
+++ server/middlewares/validators.js
@@ -1,32 +1,62 @@
 const { check } = require("express-validator");
+const { validateBirthYear, validateBirthMonth } = require("../utils/helpers");
 
 exports.registerDataValidationRules = [
   // full name
   check("first_name")
-  .trim()
-  .escape()
-  .notEmpty()
-  .withMessage("First name is required")
-  .isLength({min:1, max:30})
-  .withMessage("First name should be between 1 to 30 characters"),
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("First name is required!")
+    .isLength({ min: 1, max: 30 })
+    .withMessage("First name should be between 1 to 30 characters!"),
 
   // last name
   check("last_name")
-  .trim()
-  .escape()
-  .notEmpty()
-  .withMessage("Last name is required")
-  .isLength({min:1, max:30})
-  .withMessage("Last name should be between 1 to 30 characters"),
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("Last name is required!")
+    .isLength({ min: 1, max: 30 })
+    .withMessage("Last name should be between 1 to 30 characters!"),
 
   // email
   check("email")
-  .trim()
-  .escape()
-  .notEmpty()
-  .withMessage("Email is required")
-  .isLength({max:50})
-  .withMessage("Email should be 50 characters")
-  .isEmail()
-  .withMessage("Email is invalid"),
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("Email is required!")
+    .isLength({ max: 50 })
+    .withMessage("Email should be maximums 50 characters!")
+    .isEmail()
+    .withMessage("Email is invalid!"),
+
+  // password
+  check("password")
+    .notEmpty()
+    .withMessage("Password is required!")
+    .isStrongPassword()
+    .withMessage("Password is not strong!"),
+
+    // birth_year
+    check("birth_year")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("Birth year is required!")
+    .custom((birthYear)=>{
+      return validateBirthYear(birthYear)
+    })
+    .withMessage("Invalid birth year!"),
+
+    // birth_month
+    check("birth_month")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("Birth month is required!")
+    .custom((birthMonth)=>{
+      return validateBirthMonth(birthMonth)
+    })
+    .withMessage("Invalid birth month!"),
 ];
--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -1,4 +1,27 @@
-exports.birthYearValidation = ()=>{
-    return true
+// birth year validator
+exports.validateBirthYear = (birthYear) => {
+  // get current year
+  const currentYear = new Date().getFullYear();
 
-}
+  //   check if birthYear is a number and within a reasonable range
+  if (isNaN(birthYear) || birthYear < 1900 || birthYear > currentYear) {
+    return false; // invalid birth year
+  }
+  //birth year is valid
+  return true;
+};
+
+// birth month validator
+exports.validateBirthMonth = (birthMonth) => {
+  //   check if birthYear is a number and within a reasonable range
+  if (
+    isNaN(birthMonth) ||
+    birthMonth < 1 ||
+    birthMonth > 12 ||
+    birthMonth.length > 2
+  ) {
+    return false; // invalid birth year
+  }
+  //birth year is valid
+  return true;
+};
